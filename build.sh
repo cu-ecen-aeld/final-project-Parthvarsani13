ce4304b72a4eab543b138ade7fec83619c1be649
Parth Varsani <pava1107@aesd-students.colorado.edu>
2025-04-13 05:08:59 +0000

    rpi4: add CAN kernel support and utilities for MCP2515 setup

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script to build image for qemu.
 # Author: Siddhant Jajoo.
+# Modifier: Parth Varsani
 
 git submodule init
 git submodule sync
@@ -8,7 +9,6 @@ git submodule update
 
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
-
 
 # CONFLINE="MACHINE = \"qemuarm64\""
 CONFLINE="MACHINE = \"raspberrypi4\""
@@ -19,6 +19,18 @@ IMAGE="IMAGE_FSTYPES = \"wic.bz2\""
 #Set GPU memory as minimum
 MEMORY="GPU_MEM = \"16\""
 
+# I2C related
+MODULE_I2C="ENABLE_I2C = \"1\""
+AUTOLOAD_I2C="KERNEL_MODULE_AUTOLOAD:rpi += \"i2c-dev i2c-bcm2708\""
+
+# CAN related
+MODULE_CAN="ENABLE_CAN = \"1\""
+AUTOLOAD_CAN="KERNEL_MODULE_AUTOLOAD:rpi += \"mcp251x flexcan\""
+CAN_OSCILLATOR_CONFIG="CAN_OSCILLATOR = \"8000000\""
+
+# Extra image tools
+IMAGE_ADD="IMAGE_INSTALL:append = \" i2c-tools python3 mosquitto can-utils iproute2\""
+
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
 
@@ -28,99 +40,109 @@ local_image_info=$?
 cat conf/local.conf | grep "${MEMORY}" > /dev/null
 local_memory_info=$?
 
-MODULE_I2C="ENABLE_I2C = \"1\""
 cat conf/local.conf | grep "${MODULE_I2C}" > /dev/null
 local_i2c_info=$?
 
-# Autoload I2C_MODULE
-AUTOLOAD_I2C="KERNEL_MODULE_AUTOLOAD:rpi += \"i2c-dev i2c-bcm2708\""
 cat conf/local.conf | grep "${AUTOLOAD_I2C}" > /dev/null
 local_i2c_autoload_info=$?
 
-###############################################################
-# Add the following to add i2c-tools support for detecting 
-# I2C device
-###############################################################
-IMAGE_ADD="IMAGE_INSTALL:append = \" i2c-tools python3 mosquitto\""
+cat conf/local.conf | grep "${MODULE_CAN}" > /dev/null
+local_can_info=$?
+
+cat conf/local.conf | grep "${AUTOLOAD_CAN}" > /dev/null
+local_can_autoload_info=$?
+
+cat conf/local.conf | grep "${CAN_OSCILLATOR_CONFIG}" > /dev/null
+local_can_oscillator_info=$?
+
 cat conf/local.conf | grep "${IMAGE_ADD}" > /dev/null
 local_imgadd_info=$?
 
 if [ $local_conf_info -ne 0 ];then
-echo "Append ${CONFLINE} in the local.conf file"
-        echo ${CONFLINE} >> conf/local.conf
-
+    echo "Append ${CONFLINE} in the local.conf file"
+    echo ${CONFLINE} >> conf/local.conf
 else
-        echo "${CONFLINE} already exists in the local.conf file"
+    echo "${CONFLINE} already exists in the local.conf file"
 fi
 
 if [ $local_i2c_info -ne 0 ];then
-        echo "Append ${MODULE_I2C} in the local.conf file"
-        echo ${MODULE_I2C} >> conf/local.conf
+    echo "Append ${MODULE_I2C} in the local.conf file"
+    echo ${MODULE_I2C} >> conf/local.conf
 else
-        echo "${MODULE_I2C} already exists in the local.conf file"
+    echo "${MODULE_I2C} already exists in the local.conf file"
 fi
 
 if [ $local_i2c_autoload_info -ne 0 ];then
-        echo "Append ${AUTOLOAD_I2C} in the local.conf file"
-        echo ${AUTOLOAD_I2C} >> conf/local.conf
-
+    echo "Append ${AUTOLOAD_I2C} in the local.conf file"
+    echo ${AUTOLOAD_I2C} >> conf/local.conf
 else
-	echo "${AUTOLOAD_I2C} already exists in the local.conf file"
+    echo "${AUTOLOAD_I2C} already exists in the local.conf file"
+fi
+
+if [ $local_can_info -ne 0 ];then
+    echo "Append ${MODULE_CAN} in the local.conf file"
+    echo ${MODULE_CAN} >> conf/local.conf
+else
+    echo "${MODULE_CAN} already exists in the local.conf file"
+fi
+
+if [ $local_can_autoload_info -ne 0 ];then
+    echo "Append ${AUTOLOAD_CAN} in the local.conf file"
+    echo ${AUTOLOAD_CAN} >> conf/local.conf
+else
+    echo "${AUTOLOAD_CAN} already exists in the local.conf file"
+fi
+
+if [ $local_can_oscillator_info -ne 0 ];then
+    echo "Append ${CAN_OSCILLATOR_CONFIG} in the local.conf file"
+    echo ${CAN_OSCILLATOR_CONFIG} >> conf/local.conf
+else
+    echo "${CAN_OSCILLATOR_CONFIG} already exists in the local.conf file"
 fi
 
 if [ $local_image_info -ne 0 ];then 
     echo "Append ${IMAGE} in the local.conf file"
-	echo ${IMAGE} >> conf/local.conf
+    echo ${IMAGE} >> conf/local.conf
 else
-	echo "${IMAGE} already exists in the local.conf file"
+    echo "${IMAGE} already exists in the local.conf file"
 fi
 
 if [ $local_memory_info -ne 0 ];then
     echo "Append ${MEMORY} in the local.conf file"
-	echo ${MEMORY} >> conf/local.conf
+    echo ${MEMORY} >> conf/local.conf
 else
-	echo "${MEMORY} already exists in the local.conf file"
+    echo "${MEMORY} already exists in the local.conf file"
 fi
-
-# if [ $local_licn_info -ne 0 ];then
-#     echo "Append ${LICENCE} in the local.conf file"
-# 	echo ${LICENCE} >> conf/local.conf
-# else
-# 	echo "${LICENCE} already exists in the local.conf file"
-# fi
 
 if [ $local_imgadd_info -ne 0 ];then
-        echo "Append ${IMAGE_ADD} in the local.conf file"
-        echo ${IMAGE_ADD} >> conf/local.conf
+    echo "Append ${IMAGE_ADD} in the local.conf file"
+    echo ${IMAGE_ADD} >> conf/local.conf
 else
-        echo "${IMAGE_ADD} already exists in the local.conf file"
+    echo "${IMAGE_ADD} already exists in the local.conf file"
 fi
-
-
 
 bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
 layer_info=$?
 
 if [ $layer_info -ne 0 ];then
-	echo "Adding meta-raspberrypi  layer"
-	bitbake-layers add-layer ../meta-raspberrypi
+    echo "Adding meta-raspberrypi  layer"
+    bitbake-layers add-layer ../meta-raspberrypi
 else
-	echo "meta-raspberrypi  layer already exists"
+    echo "meta-raspberrypi  layer already exists"
 fi
-
 
 bitbake-layers show-layers | grep "meta-openembedded" > /dev/null
 layer_info_2=$?
 
 if [ $layer_info_2 -ne 0 ];then
-	echo "Adding meta-openembedded  layer"
-	bitbake-layers add-layer ../meta-openembedded/meta-oe
-	bitbake-layers add-layer ../meta-openembedded/meta-python
-	bitbake-layers add-layer ../meta-openembedded/meta-networking
+    echo "Adding meta-openembedded  layer"
+    bitbake-layers add-layer ../meta-openembedded/meta-oe
+    bitbake-layers add-layer ../meta-openembedded/meta-python
+    bitbake-layers add-layer ../meta-openembedded/meta-networking
 else
-	echo "meta-openembedded  layer already exists"
+    echo "meta-openembedded  layer already exists"
 fi
-
 
 set -e
 bitbake core-image-base
+
